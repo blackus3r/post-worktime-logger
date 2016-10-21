83216cdabf57f1fbfa35eb551a7cc6d1258594b8
Henrique Arthur <hnrq.art@gmail.com>
2016-10-21 20:18:46 -0300

    Fixed 'Titel' typo to 'Title'

--- widget.php
+++ widget.php
@@ -84,7 +84,7 @@ class PwlFrontendWidget extends WP_Widget
         $afterText = $_instance['afterText'];
         ?>
         <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('Titel:', "post-worktime-logger"); ?></label>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('Title:', "post-worktime-logger"); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
         </p>
         <p>
